081f4c628ed814db34b717960b9be14149aa5f86
BRiches423 <71953002+BRiches423@users.noreply.github.com>
2021-04-07 19:58:12 -0500

    Add files via upload

---
+++ DDL for Player.sql
@@ -0,0 +1,32 @@
+use WorldCup;
+create table Player
+	(First_Name varchar(30), Last_Name varchar(30), Nick_Name varchar(30),
+    Number int, Team_Name varchar(30));
+    
+Alter Table Player
+	ADD primary key (Number, Team_Name);
+    
+
+
+create table Player_Position
+	( Number int, TName varchar(30), Position varchar(30));
+
+Alter table Player_Position
+	Add primary key (TName, Position);
+    
+    
+Alter Table Player_Position
+	ADD foreign key (Number, TName) references Player (Number, Team_Name);
+
+Alter Table Player
+	ADD goals int;
+    
+
+create table Player_Award
+	(Number int, TName varchar(30), Award varchar(30));
+    
+alter table Player_Award
+	ADD Primary Key (TName, Award);
+    
+ALter table Player_Award
+	ADD foreign key (Number, TName) references Player (Number, Team_Name);
